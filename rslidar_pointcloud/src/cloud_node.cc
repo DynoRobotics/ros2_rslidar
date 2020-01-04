1eee2929d6befc666158509b0b6ad570eb25f7a2
Fredrik Löfgren <frelo223@student.liu.se>
2020-01-04 12:48:20 +0100

    Add file for the cloud_node

---
+++ rslidar_pointcloud/src/cloud_node.cc
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (C) 2012 Austin Robot Technology, Jack O'Quin
+ *  Copyright (C) 2016 Robosense, Tony Zhang
+ *  Copyright (C) 2019 Dyno Robotics, Fredrik Löfgren
+ *  License: Modified BSD Software License Agreement
+ *
+ *  $Id$
+ */
+
+/** \file
+
+    This ROS node converts raw RSLIDAR LIDAR packets to PointCloud2.
+
+*/
+#include "convert.h"
+
+/** Main node entry point. */
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<rslidar_pointcloud::Convert>());
+  rclcpp::shutdown();
+  return 0;
+}
